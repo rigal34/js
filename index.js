834628aa853f71e59957f433b3b48369037aa439
ALBATOR <SST.plomberie@laposte.net>
2021-12-04 00:20:31 +0100

    1er js

---
+++ index.js
@@ -0,0 +1,14 @@
+//les variables
+let memoire ="interieur de ma variable" //variable qui évolue
+console.log(memoire)
+
+console.log(memoire + ' // avant je concatéine');
+
+memoire = "interieur de ma variable suite "
+
+console.log(memoire + "aprés je concatéine");
+
+const variable = "nechangepas"; //variable qui évolue pas
+
+
+console.log(variable);
